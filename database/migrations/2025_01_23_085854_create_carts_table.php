2b96c5fae6485e76d92c2b04a3f0d802f6e513ac
Dwi Andika F <dwiezy1509@gmail.com>
2025-01-23 10:14:15 +0700

    menambahkan migrasi cart

---
+++ database/migrations/2025_01_23_085854_create_carts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\User;
+use App\Models\Product;
+use App\Models\Transaction;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('carts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class);
+            $table->foreignIdFor(Product::class);
+            $table->foreignIdFor(Transaction::class);
+            $table->integer('jumlah');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('carts');
+    }
+};
