c0e7ca6e575a87f16ada2a260c0254c0e6cf963b
Dwi Andika F <dwiezy1509@gmail.com>
2025-02-09 11:06:15 +0700

    Adding sanctum

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,8 @@ use App\Http\Middleware\RoleMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
+
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -20,3 +22,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
+
+    $app->http->middleware([
+        EnsureFrontendRequestsAreStateful::class,
+    ]);
